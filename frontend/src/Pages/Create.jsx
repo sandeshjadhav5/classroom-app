f5f27c4f6d9706865f631e85f4908a16de495686
sandeshjadhav5 <jadhavs932@gmail.com>
2023-03-06 22:18:27 +0530

    added create class ui

--- frontend/src/Pages/Create.jsx
+++ frontend/src/Pages/Create.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Admin.css";
+import addClass from "../assets/addClass.png";
 
 function Create() {
   return (
@@ -7,28 +8,33 @@ function Create() {
       <div>
         <h1>Create Class</h1>
       </div>
-      <div className="create-form">
-        <form>
-          <div>
-            <span>Class Name</span>
-            <input />
-          </div>
-          <div>
-            <span>Section</span>
-            <input />
-          </div>
-          <div>
-            <span>Subject</span>
-            <input />
-          </div>
-          <div>
-            <span>Room</span>
-            <input />
-          </div>
-          <div>
-            <input className="InputSubmitBtn" type="submit" />
-          </div>
-        </form>
+      <div className="add-class-grid">
+        <div>
+          <img src={addClass} />
+        </div>
+        <div className="create-form">
+          <form>
+            <div>
+              <span>Class Name</span>
+              <input />
+            </div>
+            <div>
+              <span>Section</span>
+              <input />
+            </div>
+            <div>
+              <span>Subject</span>
+              <input />
+            </div>
+            <div>
+              <span>Room</span>
+              <input />
+            </div>
+            <div>
+              <input className="InputSubmitBtn" type="submit" />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
